82a950ee7aa86aaa57e1509f9e26bc3e2281a209
zzach00 <hieptrong790@gmail.com>
2025-10-03 15:44:09 +0700

    cursor blink

--- Yap1/main.js
+++ Yap1/main.js
@@ -1,3 +1,53 @@
+
+
+// Custom Cursor
+const cursor = document.querySelector('.cursor');
+const cursorHigh = document.querySelector('.cursor-high');
+const hoverables = document.querySelectorAll('[data-hoverable]');
+
+const onMouseMove =(e) => {
+    const { clientX, clientY } = e;
+    gsap.to(cursor, {
+        x: clientX,
+        y: clientY,
+        duration: 1.9,
+        ease: "power2.out"
+    })
+    gsap.to(cursorHigh, {
+        x: clientX,
+        y: clientY,
+        duration: 0,
+    })
+};
+
+const onMouseEnter = () => {
+    gsap.to(cursor,{
+        scale: 2,
+        duration: 0.3,
+        ease: "slow.out",
+        rotation: 45,
+        borderRadius: '25%',
+    })
+}
+
+const onMouseLeave = () => {
+    gsap.to(cursor,{
+        scale: 1,
+        duration: 0.3,
+        ease: "slow.in",
+        rotation: 0,
+        borderRadius: '50%',
+    })
+}
+
+window.addEventListener('mousemove', onMouseMove);
+hoverables.forEach((card) => {
+    card.addEventListener('mouseenter', onMouseEnter);
+    card.addEventListener('mouseleave', onMouseLeave);
+})
+
+
+
 const counter = {value: 0};
 
 const tl = gsap.timeline(
@@ -26,7 +76,7 @@ tl.to(
         duration: 1.5,
         ease: "slow",
         oncomplete: () => {
-            document.body.style.overflow = 'auto';
+            document.body.style.overflow = 'hidden';
         }
     }
 );
@@ -38,3 +88,8 @@ tl.fromTo('.main-section', {
     duration: 1,
 },
 "<");
+
+tl.call(() => {
+    document.body.style.overflow = 'auto';
+})
+
